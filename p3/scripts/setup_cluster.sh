6165b61f6b253b315ba1756ec85145293626d273
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-10-29 16:17:03 +0100

    feat: test about loadbalancing

--- p3/scripts/setup_cluster.sh
+++ p3/scripts/setup_cluster.sh
@@ -2,10 +2,12 @@
 set -e
 
 k3d cluster delete -a
-k3d cluster create cluster-p3 --port "8080:80@loadbalancer"
+k3d cluster create cluster-p3 --port "8443:443@loadbalancer" 
 
 # Argo cd
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl wait --namespace argocd --for=condition=ready pod --all --timeout=120s
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
