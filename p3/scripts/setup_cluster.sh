5897687a7fbc74d6872171f98eff9ae45249a9df
Slqye <69047488+slqye@users.noreply.github.com>
2025-10-29 16:26:35 +0100

    feat: starting ingress

--- p3/scripts/setup_cluster.sh
+++ p3/scripts/setup_cluster.sh
@@ -2,12 +2,11 @@
 set -e
 
 k3d cluster delete -a
-k3d cluster create cluster-p3 --port "8443:443@loadbalancer" 
+k3d cluster create cluster-p3
 
 # Argo cd
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl wait --namespace argocd --for=condition=ready pod --all --timeout=120s
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-
+kubectl apply -f confs/ingress.yaml
