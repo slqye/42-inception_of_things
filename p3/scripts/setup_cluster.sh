fa19144170ffe297134cbaeddbdba71994a93727
Slqye <69047488+slqye@users.noreply.github.com>
2025-10-29 16:12:46 +0100

    feat: starting argocd setup

--- p3/scripts/setup_cluster.sh
+++ p3/scripts/setup_cluster.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
-k3d cluster create cluster-p3
-kubectl get all
+k3d cluster delete -a
+k3d cluster create cluster-p3 --port "8080:80@loadbalancer"
+
+# Argo cd
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl wait --namespace argocd --for=condition=ready pod --all --timeout=120s
