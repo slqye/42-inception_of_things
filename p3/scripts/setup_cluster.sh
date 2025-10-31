29989606cc690e7a5891bb15f0c29bde8eda7e86
user.name=Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-10-31 14:15:45 +0100

    chore: color for better readability

--- p3/scripts/setup_cluster.sh
+++ p3/scripts/setup_cluster.sh
@@ -31,7 +31,7 @@ INITIAL_PASSWORD=$(argocd admin initial-password -n argocd | head -n 1)
 argocd login argocd.sh --username admin --password $INITIAL_PASSWORD --grpc-web --insecure
 NEW_PASSWORD=$(openssl rand -hex 16)
 argocd account update-password --current-password $INITIAL_PASSWORD --new-password $NEW_PASSWORD --grpc-web --insecure
-echo "Admin password updated to: $NEW_PASSWORD"
+echo -e "Admin password updated to: \033[96m$NEW_PASSWORD\033[0m"
 
 ## Argo cd CLI - set argocd app
 kubectl apply -f confs/argocd/application.yaml
