6b652ae27e506eb853ee3dd54b97abb1cb468aee
user.name=Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-10-31 14:02:30 +0100

    feat: argocd properly set up to follow application repo

--- p3/scripts/setup_cluster.sh
+++ p3/scripts/setup_cluster.sh
@@ -4,9 +4,11 @@ set -e
 k3d cluster delete -a
 k3d cluster create cluster-p3 -p "443:443@loadbalancer" -p "80:80@loadbalancer"
 
-# Argo cd
 kubectl create namespace argocd
-kubectl apply -f confs/argocd/install.yaml -n argocd
+kubectl create namespace dev
+
+# Argo cd
+kubectl apply -f confs/argocd/argocd-install.yaml -n argocd
 echo "=== Waiting for Argo CD to be ready ==="
 kubectl wait --namespace argocd --for=condition=ready pod --all --timeout=120s
 
@@ -29,13 +31,8 @@ INITIAL_PASSWORD=$(argocd admin initial-password -n argocd | head -n 1)
 argocd login argocd.sh --username admin --password $INITIAL_PASSWORD --grpc-web --insecure
 NEW_PASSWORD=$(openssl rand -hex 16)
 argocd account update-password --current-password $INITIAL_PASSWORD --new-password $NEW_PASSWORD --grpc-web --insecure
-argocd logout argocd.sh
 echo "Admin password updated to: $NEW_PASSWORD"
 
-## Argo cd CLI - set app
-#kubectl config set-context --current --namespace=argocd
-
-kubectl create namespace dev
-kubectl apply -f confs/dev/deployment.yaml -n dev # replace this later with argocd auto-sync and deployment.yaml from repo
-kubectl apply -f confs/dev/service.yaml -n dev
-kubectl apply -f confs/dev/ingress.yaml -n dev
+## Argo cd CLI - set argocd app
+kubectl apply -f confs/argocd/application.yaml
+argocd logout argocd.sh
