40d0dde54c74a6c29731c38815907072336257bf
Slqye <69047488+slqye@users.noreply.github.com>
2025-11-04 15:15:17 +0100

    fix: typo

--- bonus/scripts/cluster.sh
+++ bonus/scripts/cluster.sh
@@ -47,7 +47,7 @@ INITIAL_PASSWORD=$(argocd admin initial-password -n argocd | head -n 1)
 run argocd login argocd.sh --username admin --password $INITIAL_PASSWORD --grpc-web --insecure
 NEW_PASSWORD=$(openssl rand -hex 16)
 run argocd account update-password --current-password $INITIAL_PASSWORD --new-password $NEW_PASSWORD --grpc-web --insecure
-echo "new password: $NEW_PASSWORD"
+echo "Password: $NEW_PASSWORD"
 
 echo "Setup of argocd app"
 run kubectl apply -f confs/argocd/application.yaml
